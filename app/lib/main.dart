0a7c7c346281e7f5698153cd612aff6569c79f95
Jason Burkybile <jburky@gmail.com>
2018-10-24 15:27:47 -0400

    Wired up the bottom nav.

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -11,25 +11,37 @@ class MobileApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Keeneland',
-      theme: ThemeData(
-        splashColor: keenelandGreen,
-        accentColor: Colors.white,
-        primaryColor: keenelandGreen,
-        canvasColor: keenelandGreen,
-        brightness: Brightness.dark,
-        backgroundColor: keenelandGreen,
-      ),
-      debugShowCheckedModeBanner: false,
-      home: CustomSplashScreen(
-        backgroundColor: keenelandGreen,
-        loadingSplash: SplashPage(),
-        errorSplash: ErrorPage(),
-        seconds: 3,
-        navigateAfterSeconds: HomePage(),
-      ),
-    );
+    return ScopedModel<AppModel>(
+        model: new AppModel(),
+        child: MaterialApp(
+          title: 'Keeneland',
+          theme: ThemeData(
+            splashColor: keenelandGreen,
+            accentColor: Colors.white,
+            primaryColor: keenelandGreen,
+            canvasColor: keenelandGreen,
+            brightness: Brightness.dark,
+            backgroundColor: keenelandGreen,
+          ),
+          debugShowCheckedModeBanner: false,
+          home: CustomSplashScreen(
+            backgroundColor: keenelandGreen,
+            loadingSplash: SplashPage(),
+            errorSplash: ErrorPage(),
+            seconds: 3,
+            navigateAfterSeconds: HomePage(),
+          ),
+        ));
+  }
+}
+
+class AppModel extends Model {
+  int _selectedPageIndex = 0;
+  int get selectedPageIndex => _selectedPageIndex;
+
+  void selectPage(int index) {
+    _selectedPageIndex = index;
+    notifyListeners();
   }
 }
 
@@ -56,39 +68,96 @@ class SplashPage extends StatelessWidget {
   }
 }
 
-class HomePage extends StatelessWidget {
+class HomePage extends ScopedModelDescendant<AppModel> {
+  @override
+  // TODO: implement builder
+  ScopedModelDescendantBuilder<AppModel> get builder =>
+      (context, child, model) => Scaffold(
+            backgroundColor: keenelandGreen,
+            appBar: AppBar(
+                // title: Text('Keenland'),
+                title: Image.asset('graphics/logo-branded-wide.png',
+                    fit: BoxFit.cover)),
+            body: Container(
+                decoration: new BoxDecoration(
+                  image: new DecorationImage(
+                    image: new AssetImage('graphics/background-small.png'),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                child: [
+                  Container(
+                    child: Center(child: Text('Home Page stuff...')),
+                  ),
+                  Swiper(
+                    itemBuilder: (BuildContext context, int index) {
+                      return new Image.network(
+                        "https://loremflickr.com/350/500/horse,racing/all?random=${index})",
+                        fit: BoxFit.fill,
+                      );
+                    },
+                    itemCount: 10,
+                    itemWidth: 350.0,
+                    itemHeight: 500.0,
+                    layout: SwiperLayout.TINDER,
+                  )
+                ][model.selectedPageIndex]),
+            bottomNavigationBar: BottomNavigationBar(
+              currentIndex: model.selectedPageIndex,
+              onTap: (index) => model.selectPage(index),
+              items: <BottomNavigationBarItem>[
+                BottomNavigationBarItem(
+                    title: Text('Home'), icon: Icon(Icons.home)),
+                BottomNavigationBarItem(
+                    title: Text('Book'), icon: Icon(Icons.library_books))
+              ],
+            ),
+          );
+}
+
+// class HomePage extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return new Scaffold(
+//       backgroundColor: keenelandGreen,
+//       appBar: AppBar(
+//           // title: Text('Keenland'),
+//           title:
+//               Image.asset('graphics/logo-branded-wide.png', fit: BoxFit.cover)),
+//       body: new Container(
+//           decoration: new BoxDecoration(
+//             image: new DecorationImage(
+//               image: new AssetImage('graphics/background-small.png'),
+//               fit: BoxFit.cover,
+//             ),
+//           ),
+//           child: new Swiper(
+//             itemBuilder: (BuildContext context, int index) {
+//               return new Image.network(
+//                 "https://loremflickr.com/350/500/horse,racing/all?random=${index})",
+//                 fit: BoxFit.fill,
+//               );
+//             },
+//             itemCount: 10,
+//             itemWidth: 350.0,
+//             itemHeight: 500.0,
+//             layout: SwiperLayout.TINDER,
+//           )),
+//       bottomNavigationBar: BottomNavigationBar(
+//         items: <BottomNavigationBarItem>[
+//           BottomNavigationBarItem(title: Text('Home'), icon: Icon(Icons.home)),
+//           BottomNavigationBarItem(
+//               title: Text('Book'), icon: Icon(Icons.library_books))
+//         ],
+//         onTap: (index) => {},
+//       ),
+//     );
+//   }
+// }
+
+class BookPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(
-      backgroundColor: keenelandGreen,
-      appBar: AppBar(
-          // title: Text('Keenland'),
-          title:
-              Image.asset('graphics/logo-branded-wide.png', fit: BoxFit.cover)),
-      body: new Container(
-          decoration: new BoxDecoration(
-            image: new DecorationImage(
-              image: new AssetImage('graphics/background-small.png'),
-              fit: BoxFit.cover,
-            ),
-          ),
-          child: new Swiper(
-            itemBuilder: (BuildContext context, int index) {
-              return new Image.network(
-                "https://loremflickr.com/350/500/horse,racing/all?random=${index})",
-                fit: BoxFit.fill,
-              );
-            },
-            itemCount: 10,
-            itemWidth: 350.0,
-            itemHeight: 500.0,
-            layout: SwiperLayout.TINDER,
-          )),
-      bottomNavigationBar: BottomNavigationBar(items: <BottomNavigationBarItem>[
-        BottomNavigationBarItem(title: Text('Home'), icon: Icon(Icons.home)),
-        BottomNavigationBarItem(
-            title: Text('Book'), icon: Icon(Icons.library_books))
-      ]),
-    );
+    return new Container();
   }
 }
